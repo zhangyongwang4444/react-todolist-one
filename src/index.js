b22654bc1ec7355c40a89128081fca1e4bbc0c72
zhangyongwang <20225614@qq.com>
2019-04-08 16:16:56 +0800

    引入jQuery

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
+import j from 'jquery'
 function component() {
-    let element = document.createElement('div');
+    // let element = document.createElement('div');
+    let element = j('<div></div>')
 
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.html(_.join(['Hello', 'webpack'], ' '))
+
+    // return element;
+    return element.get(0)
 }
 
 document.body.appendChild(component());
