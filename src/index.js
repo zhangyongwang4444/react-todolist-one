6362cf759daae5103aa8e78ee4cbeb8faf6566e0
zhangyongwang <20225614@qq.com>
2019-04-09 10:43:55 +0800

    引入welcome.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <h1>Hello, world!</h1>, document.getElementById('root'));
+
+import Welcome from './welcome'
+
+ReactDOM.render(
+    <Welcome />,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
