94784d334813facf347210c5aaf42751d0a4e147
zhangyongwang <20225614@qq.com>
2019-04-10 10:04:28 +0800

    将输入框变成TodoInput组件

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import TodoInput from './TodoInput'
 
 class App extends Component {
   constructor(props) {
@@ -20,7 +21,7 @@ class App extends Component {
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <input type="text" value={this.state.newTodo}></input>
+          <TodoInput content={this.state.newTodo} />
         </div>
         <ol>
           {todos}
